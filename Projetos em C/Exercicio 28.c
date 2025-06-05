f26a3a138de267aa77ec542a911aebe4b62b24db
BiancaPiccini <bianca_campos1913@hotmail.com>
2025-06-04 20:13:06 -0400

    Update Exercicio 28.c
    Alterado nome variável

--- Projetos em C/Exercicio 28.c
+++ Projetos em C/Exercicio 28.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int inteiro1, inteiro2;
-    float real;
+    float inteiro3;
 
     printf("Digite o primeiro numero inteiro: ");
     scanf("%d", &inteiro1);
@@ -11,11 +12,11 @@ int main() {
     scanf("%d", &inteiro2);
 
     printf("Digite um numero real: ");
-    scanf("%f", &real);
+    scanf("%f", &inteiro3);
 
     float resultado1 = (2 * inteiro1) * (inteiro2 / 2.0);
-    float resultado2 = (3 * inteiro1) + real;
-    float resultado3 = real * real * real;
+    float resultado2 = (3 * inteiro1) + inteiro3;
+    float resultado3 = inteiro3 * inteiro3 * inteiro3;
 
     printf("Produto do dobro do primeiro com metade do segundo: %.2f\n", resultado1);
     printf("Soma do triplo do primeiro com o terceiro: %.2f\n", resultado2);
